aa97318edb7e8bfc03d0f45b3bc0dd261a824e65
joey0222 <joeyjoey0222@gmail.com>
2019-06-07 17:21:33 -0500

    Food stuff

--- Bad civ/src/playerInfo/Player.java
+++ Bad civ/src/playerInfo/Player.java
@@ -8,12 +8,40 @@ public class Player {
 	public int ore;
 	public int food;
 	public int population;
+	public int normies;
+	public int soldiers;
 	
-	public void tick() {
-		
+	public Player(int gold, int ore, int food, int population) {
+		this.gold = gold;
+		this.ore = ore;
+		this.food = food;
+		this.population = population;
+		this.normies = population;
+		this.soldiers = 0;
 	}
 	
-	public void render() {
-		
+	public Player() {
+		this(0, 0, 0, 0);
 	}
+	
+	public void calcGrowth() {
+		int growth = 0;
+		if (food > foodConsumption()) {
+			growth++;
+		}else if (food == foodConsumption()) {
+			growth = 0;
+		}else {
+			growth = -1;
+		}
+		population = population + growth;
+		food -= foodConsumption();
+	}
+	
+	public int foodConsumption() {
+		int foodConsumed = 0;
+		foodConsumed += normies * 10;
+		foodConsumed += soldiers * 20;
+		return foodConsumed;
+	}
+	
 }
